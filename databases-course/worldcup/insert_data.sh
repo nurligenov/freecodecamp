9d1acf1a686c67ab89ffdbc5ba8e91f959c5ead4
nurligenov <66307255+nurligenov@users.noreply.github.com>
2024-01-13 19:31:27 +0600

    Create insert_data.sh

---
+++ databases-course/worldcup/insert_data.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "truncate games, teams;")
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $YEAR != year ]]
+  then
+    #get winner_id
+    WINNER_ID=$($PSQL "select team_id from teams where name = '$WINNER';")
+    #if not found
+    if [[ -z $WINNER_ID ]]
+    then
+          #insert team
+          INSERT_WINNER_RESULT=$($PSQL "INSERT INTO teams(name) values ('$WINNER');")
+          
+          if [[ $INSERT_WINNER_RESULT == "INSERT 0 1" ]] 
+          then
+            echo Inserted into teams, $WINNER
+          fi
+          WINNER_ID=$($PSQL "select team_id from teams where name = '$WINNER';")
+    fi
+
+    #get opponent_id
+    OPPONENT_ID=$($PSQL "select team_id from teams where name = '$OPPONENT';")
+    #if not found
+    if [[ -z $OPPONENT_ID ]]
+    then
+          #insert team
+          INSERT_OPPONENT_RESULT=$($PSQL "INSERT INTO teams(name) values ('$OPPONENT');")
+          
+          if [[ $INSERT_OPPONENT_RESULT == "INSERT 0 1" ]] 
+          then
+            echo Inserted into teams, $OPPONENT
+          fi
+          OPPONENT_ID=$($PSQL "select team_id from teams where name = '$OPPONENT';")
+    fi
+
+    # insert games
+    INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")
+    if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]] 
+    then
+      echo Inserted into games, $WINNER : $OPPONENT
+    fi
+          
+  fi
+done
